6c9a6ee7ff0f1655cdd96c4302487b583521c16c
WeiseSchokolade <weiseschokolade@weiseschokolade.github.io>
2023-02-20 13:01:12 +0100

    Added setBackgroundColor(r, g, b)

--- src/de/schoko/rendering/RendererSettings.java
+++ src/de/schoko/rendering/RendererSettings.java
@@ -41,8 +41,23 @@ public class RendererSettings {
 		return backgroundColor;
 	}
 	
+	/**
+	 * Sets the background of the window to a {@link java.awt.Color}
+	 * @param backgroundColor The color
+	 */
 	public void setBackgroundColor(Color backgroundColor) {
 		this.backgroundColor = backgroundColor;
+	}
+	
+	/**
+	 * Sets the background of the window to a color in rgb color space.
+	 * @param r Red Color Amount (Value between 0 and 255)
+	 * @param g Green Color Amount (Value between 0 and 255)
+	 * @param b Blue Color Amount (Value between 0 and 255)
+	 */
+	public void setBackgroundColor(int r, int g, int b) {
+		float[] color = Color.RGBtoHSB(r, g, b, null);
+		backgroundColor = Color.getHSBColor(color[0], color[1], color[2]);
 	}
 	
 	public boolean isCrashingOnException() {
