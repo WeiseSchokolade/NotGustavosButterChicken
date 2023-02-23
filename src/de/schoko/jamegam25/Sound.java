e6e472a7898399e54718e10363f0ab7417b87ff7
WeiseSchokolade <weiseschokolade@weiseschokolade.github.io>
2023-02-23 11:12:32 +0100

    Added exception handling to Sound Class

--- src/de/schoko/jamegam25/Sound.java
+++ src/de/schoko/jamegam25/Sound.java
@@ -1,36 +1,39 @@
 package de.schoko.jamegam25;
 
 import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Sound {
 	private Clip clip;
 
 	/**
 	 * @param path Path to location in jar
-	 * @throws IOException
-	 * @throws UnsupportedAudioFileException
-	 * @throws LineUnavailableException
 	 */
-	public Sound(String path) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
-		clip = AudioSystem.getClip();
-		InputStream stream = new BufferedInputStream(Sound.class.getClassLoader().getResourceAsStream(path));
-		AudioInputStream inputStream = AudioSystem.getAudioInputStream(stream);
-		clip.open(inputStream);
+	public Sound(String path) {
+		try {
+			clip = AudioSystem.getClip();
+			InputStream stream = new BufferedInputStream(Sound.class.getClassLoader().getResourceAsStream(path));
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(stream);
+			clip.open(inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+			clip = null;
+		}
 	}
 
 	public void start() {
-		clip.start();
+		if (clip != null) {
+			clip.start();
+		}
 	}
 
 	public void stop() {
-		clip.stop();
+		if (clip != null) {
+			clip.stop();
+		}
 	}
 }
